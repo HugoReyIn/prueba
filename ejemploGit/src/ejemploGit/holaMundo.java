4bef0990925bcbdfe35521c29feeef7eb1678778
HugoReyIn <145749183+HugoReyIn@users.noreply.github.com>
2023-09-27 08:33:43 +0200

    Update holaMundo.java

--- ejemploGit/src/ejemploGit/holaMundo.java
+++ ejemploGit/src/ejemploGit/holaMundo.java
@@ -3,7 +3,7 @@ package ejemploGit;
 public class holaMundo {
 	
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo!!");
+		System.out.println("Hola Mundo, editado desde la web");
 	}
 
 }
